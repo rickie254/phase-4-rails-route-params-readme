4c41299d9160f69132d9239e7a3fd3f142fae95c
rickie <rickiemaweu@gmail.com>
2022-09-30 15:01:59 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,13 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    if params[:id].to_i > Cheese.last.id
+      render json: {error:"We can't find what you are looking for"}
+    else
+      cheese = Cheese.find(params[:id])
+      render json: cheese
+    end
+  end
+
 end
